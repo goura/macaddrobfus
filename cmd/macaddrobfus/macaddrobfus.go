e46200fc65ffd94fd96e27a05c596fc63ce09c51
Kazuhiro Ogura <goura@goura.com>
2022-04-30 21:41:17 +0900

    Add option to specify secret

--- cmd/macaddrobfus/macaddrobfus.go
+++ cmd/macaddrobfus/macaddrobfus.go
@@ -2,13 +2,45 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"flag"
+	"math/rand"
 	"os"
+	"time"
 
 	"github.com/rgoura/macaddrobfus/internal/stream"
 )
 
+func randomString() string {
+	var buf bytes.Buffer
+
+	// Initialize seed with time
+	rand.Seed(time.Now().UnixMicro())
+
+	// Pick 32 random letters
+	const letters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	for i := 0; i < 32; i++ {
+		idx := rand.Intn(len(letters))
+		buf.WriteByte(letters[idx])
+	}
+	return buf.String()
+}
+
 func main() {
+	// Define flags
+	flagSecret := flag.String("secret", "not a secret", "secret key used to obfuscate strings")
+	flag.Parse()
+
+	// If secret is not specified, generate it randomely
+	var secret string
+	if *flagSecret == "not a secret" {
+		secret = randomString()
+	} else {
+		secret = *flagSecret
+	}
+
+	// Do the magic
 	in, out := bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout)
-	st := stream.New(in, out, "secret")
+	st := stream.New(in, out, secret)
 	st.ReadWrite()
 }
